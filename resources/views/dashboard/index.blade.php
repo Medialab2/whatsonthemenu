647d87db65deeb3ca4002cd25fcd6a146b0b6ddc
jordy770 <jordy.van.santen@hotmail.com>
2020-06-19 13:26:33 +0200

    Update index.blade.php
    Cleanup dashboard/index

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -25,17 +25,6 @@
                     <a class="btn btn-secondary btn-lg btn-block">Restaurant page</a>
                 </div>
             </div>
-        {{--<div class="card">
-                <div class="card-header">Dashboard</div>
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    You are logged in!
-                </div>
-            </div> --}}
         </div>
         <div class="col-md-4 dashboard-status">
             <h2> Status</h2>
